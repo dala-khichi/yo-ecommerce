3b446fb126abbc90b8c14b5d4244217beb6dde0b
Adarsh <kumaradarsh00572@gmail.com>
2025-10-16 10:11:06 +0530

    fiexd create route and edite update route

--- api/src/site/models/Address.js
+++ api/src/site/models/Address.js
@@ -27,20 +27,39 @@ class Aaddresses {
         return this.getById(id);
     }
 
-    static async create({ full_name ,  phone_number ,  alternate_phone ,  address_line1 ,  address_line2 ,  city ,  state ,  pincode ,  address_type ,  is_default },userId) {
-        const query = `INSERT INTO  addresses ( user_id ,  full_name ,  phone_number ,  alternate_phone ,  address_line1 ,  address_line2 ,  city ,  state ,  pincode ,  address_type ,  is_default , )
-         VALUES (?,?,?,?,?,?,?,?,?,?,?)`;
-         console.log(full_name ,  phone_number ,  alternate_phone ,  address_line1 ,  address_line2 ,  city ,  state ,  pincode )
+   static async create({ full_name, phone_number, alternate_phone, address_line1, address_line2, city, state, pincode,select_address_type, address_type="home", is_default=0 }, userId) {
+    const query = `
+        INSERT INTO addresses (
+            user_id, full_name, phone_number, alternate_phone, 
+            address_line1, address_line2, city, state, pincode, 
+            address_type, is_default
+        )
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `;
+
+    const [result] = await db.execute(query, [
+        userId, full_name, phone_number, alternate_phone,
+        address_line1, address_line2, city, state, pincode,
+        select_address_type, is_default
+    ]);
+
+    return result.insertId;
+}
 
 
-        const [result] = await db.execute(query, [ userId ,  full_name ,  phone_number ,  alternate_phone ,  address_line1 ,  address_line2 ,  city ,  state ,  pincode ,  address_type ,  is_default ,]);
-        return result.insertId;
-    }
-
-    static async update(id, { order_id, address, city, state, pincode, }) {
-        const query = `UPDATE addresses SET  address = ?, city = ?, state = ?, pincode = ?, 
-                        WHERE id = ?`;
-        const [result] = await db.execute(query, [address, city, state, pincode, id]);
+    static async update(id, { full_name, phone_number, alternate_phone, address_line1,
+         address_line2, city, state,
+         pincode,select_address_type, address_type="home", is_default=0 }, userId) 
+         
+         {
+        const query = `UPDATE addresses SET   full_name=?, phone_number=?, alternate_phone=?, address_line1=?,
+         address_line2=?, city=?, state=?, pincode=?, address_type=?, is_default= ? WHERE id = ?`;
+        const [result] = await db.execute(query, [
+            
+            userId, full_name, phone_number, alternate_phone,
+        address_line1, address_line2, city, state, pincode,
+        select_address_type, is_default,id
+]);
         return result.affectedRows;
     }
 
